befc57dbaa8318c44d2986585cf22a93bffbc14a
Tobias Walter <tob-walter@gmx.de>
2025-09-09 19:23:06 +0200

    init project and database

---
+++ src/main/java/de/bayern/bvv/geotopo/openstreetmapqualityframework/OpenstreetmapQualityFrameworkApplication.java
@@ -0,0 +1,13 @@
+package de.bayern.bvv.geotopo.openstreetmapqualityframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenstreetmapQualityFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenstreetmapQualityFrameworkApplication.class, args);
+	}
+
+}
