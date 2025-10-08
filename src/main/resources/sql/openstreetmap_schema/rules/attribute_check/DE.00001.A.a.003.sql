6261b7dda763dd84be567fa60b67793bc6d752b8
Tobias Walter <tob-walter@gmx.de>
2025-10-08 11:33:40 +0200

    add test case DE.00001.A.a.003

---
+++ src/main/resources/sql/openstreetmap_schema/rules/attribute_check/DE.00001.A.a.003.sql
@@ -0,0 +1,9 @@
+INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error_text) VALUES (
+    'DE.00001.A.a.003',
+    'attribute-check',
+    'AA_Objekt',
+    '{
+        "checks": { "type": "regex_match", "tag_key": "identifikator:UUID", "pattern": "(^DE(BW|BU|BY|ST|SL|SH|NI|BE|BB|NW|RP|HE|MV|SN|HH|HB|TH)[A-Za-z0-9]{12})|(^DEBKG[A-Za-z0-9]{11})|(^DE_[A-Za-z0-9]{13}$)" }
+    }',
+    'Das Tag identifikator:UUID dem regulären Ausdruck (^DE(BW|BU|BY|ST|SL|SH|NI|BE|BB|NW|RP|HE|MV|SN|HH|HB|TH)[A-Za-z0-9]{12})|(^DEBKG[A-Za-z0-9]{11})|(^DE_[A-Za-z0-9]{13}$) entsprechen.')
+ON CONFLICT (id) DO NOTHING;
