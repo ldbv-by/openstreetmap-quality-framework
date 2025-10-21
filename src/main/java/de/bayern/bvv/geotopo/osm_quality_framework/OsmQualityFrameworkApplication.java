ab2f0a851f2997b8d4f677593df615bddb37b178
Tobias Walter <tob-walter@gmx.de>
2025-10-21 09:15:59 +0200

    add caching in openstreetmap-schema

--- src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/OsmQualityFrameworkApplication.java
+++ src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/OsmQualityFrameworkApplication.java
@@ -2,9 +2,11 @@ package de.bayern.bvv.geotopo.osm_quality_framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.modulith.Modulithic;
 
 @Modulithic(additionalPackages = "de.bayern.bvv.geotopo.osm_quality_framework.quality_services")
+@EnableCaching
 @SpringBootApplication
 public class OsmQualityFrameworkApplication {
 	public static void main(String[] args) {
--- src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/openstreetmap_schema/service/OsmSchemaServiceImpl.java
+++ src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/openstreetmap_schema/service/OsmSchemaServiceImpl.java
@@ -6,15 +6,18 @@ import de.bayern.bvv.geotopo.osm_quality_framework.openstreetmap_schema.reposito
 import de.bayern.bvv.geotopo.osm_quality_framework.openstreetmap_schema.api.OsmSchemaService;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.object_type.mapper.ObjectTypeMapper;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.object_type.model.ObjectType;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Service for OpenStreetMap schema.
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OsmSchemaServiceImpl implements OsmSchemaService {
 
     private final ObjectTypeRepository objectTypeRepository;
@@ -23,11 +26,16 @@ public class OsmSchemaServiceImpl implements OsmSchemaService {
      * Get Object Type Info.
      */
     @Override
-    @Transactional
+    @Transactional(readOnly = true)
+    @Cacheable(cacheNames = "cacheObjectTypeInfo", key="#objectType", unless = "#result == null")
     public ObjectTypeDto getObjectTypeInfo(String objectType) {
+        long loadStartTime = System.currentTimeMillis();
         ObjectType obj = this.objectTypeRepository.findByObjectType(objectType)
                 .map(ObjectTypeEntityMapper::toDomain)
                 .orElse(null);
+
+        log.info("getObjectTypeInfo(): object-type={}, checkSchemaTime={} ms",
+                objectType, System.currentTimeMillis() - loadStartTime);
 
         return ObjectTypeMapper.toDto(obj);
     }
