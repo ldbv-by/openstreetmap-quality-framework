810c9d49e6546819b8d1844cd205ff9985f6056e
Tobias Walter <tob-walter@gmx.de>
2025-12-07 17:34:13 +0100

    Add test case DE.51006.G.b.002

--- src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.51006.G.b.002.sql
+++ src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.51006.G.b.002.sql
@@ -8,13 +8,30 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
             "type": "spatial_compare",
             "operator": "covered_by",
             "data_set_filter": {
+                "aggregator": "union",
                 "criteria": {
                     "any": [
                         { "type": "tag_equals", "tag_key": "object_type", "value": "AX_FlaecheBesondererFunktionalerPraegung" },
                         {
                             "all": [
                                 { "type": "tag_equals", "tag_key": "object_type", "value": "AX_SportFreizeitUndErholungsflaeche" },
-                                { "type": "tag_in", "tag_key": "funktion", "values": [ "4100", "4140", "4170", "4320" ] }
+                                { "type": "tag_in", "tag_key": "funktion", "values": [ "4100", "4140", "4170" ] }
+                            ]
+                        },
+                        {
+                            "all": [
+                                { "type": "tag_equals", "tag_key": "object_type", "value": "AX_SportFreizeitUndErholungsflaeche" },
+                                { "type": "tag_equals", "tag_key": "funktion", "value": "4320" },
+                                {
+                                    "type": "relation_exists",
+                                    "criteria": {
+                                        "all": [
+                                            { "type": "tag_equals", "tag_key": "object_type", "value": "AA_zeigtAufExternes" },
+                                            { "type": "tag_regex_match", "tag_key": "art", "pattern": "^urn:[A-Za-z]+:fdv:2600$" },
+                                            { "type": "tag_equals", "tag_key": "fachdatenobjekt:name", "value": "221430 LN_Sportanlage SPO1040 Schwimmen" }
+                                        ]
+                                    }
+                                }
                             ]
                         }
                     ]
@@ -22,5 +39,5 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
             }
         }
     }',
-    'Die Wertearten mit der ''bauwerksfunktion'' 1441 und 1442 müssen ''AX_FlaecheBesondererFunktionalerPraegung'' oder ''AX_SportFreizeitUndErholungsflaeche'' mit ''funktion'' 4100, 4140, 4170 oder 4320 überlagern.')
+    'Ein Stadion darf sich nur auf Fläche besonderer funktionaler Prägung oder Sport-, Freizeit- und Erholungsfläche mit den Funktionen Sportanlage, Reitsport, Tennis, Schwimmen (mit FDV zum Leistungsschwimmen) befinden.')
 ON CONFLICT (id) DO NOTHING;
