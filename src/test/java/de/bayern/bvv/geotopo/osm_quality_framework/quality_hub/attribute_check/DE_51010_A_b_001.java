ef1969acae65200db7c3eb1bcada4adc9b82c570
Tobias Walter <tob-walter@gmx.de>
2025-12-08 17:47:21 +0100

    Add test case DE.51010.A.b.001

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/attribute_check/DE_51010_A_b_001.java
@@ -0,0 +1,111 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.attribute_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * Die Attributart 'Kilometerangabe' kann nur in Verbindung mit der
+ * Attributart 'Art' und der Werteart 1410 vorkommen.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_51010_A_b_001 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("attribute-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.51010.A.b.001"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createKilometersteinMitKilometerangabe() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25364' changeset='-1' lat='49.88488281818' lon='12.32196506929'>
+                    <tag k='object_type' v='AX_EinrichtungInOeffentlichenBereichen' />
+                    <tag k='art' v='1410' />
+                    <tag k='kilometerangabe' v='5' />
+                  </node>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createKilometersteinOhneKilometerangabe() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25364' changeset='-1' lat='49.88488281818' lon='12.32196506929'>
+                    <tag k='object_type' v='AX_EinrichtungInOeffentlichenBereichen' />
+                    <tag k='art' v='1410' />
+                  </node>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+}
