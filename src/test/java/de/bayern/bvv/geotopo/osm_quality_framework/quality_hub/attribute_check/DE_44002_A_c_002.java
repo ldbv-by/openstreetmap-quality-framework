a0af47c19a517f7310eb8b98f0642ebcccfa1a60
Tobias Walter <tob-walter@gmx.de>
2025-12-09 10:29:38 +0100

    Add test case DE.44002.A.c.002

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/attribute_check/DE_44002_A_c_002.java
@@ -0,0 +1,239 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.attribute_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * Die Attributart 'Widmung' wird nur belegt, wenn die Attributart 'Gewässerkennzahl' belegt ist und/oder wenn die zum
+ * ZUSO 44002 'Wasserlauf' gehörenden REO 44004 'Gewässerachse' nicht die Attributart 'Hydrologisches Merkmal' führen
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_44002_A_c_002 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("attribute-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.44002.A.c.002"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createWasserlaufMitWidmungMitGewaesserkennzahl() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                <create>
+                    <node id="-1" lon="12.33103749863201" lat="49.87911888618271" version="0"/>
+                    <node id="-2" lon="12.331030993733307" lat="49.878754209010715" version="0"/>
+                    <node id="-4" lon="12.331021236757456" lat="49.87835809096834" version="0"/>
+                    <node id="-7" lon="12.33103432094183" lat="49.87894073908409" version="0"/>
+                    <node id="-9" lon="12.331024850360901" lat="49.87850479802609" version="0"/>
+                    <way id="-1" version="0">
+                        <nd ref="-1"/>
+                        <nd ref="-7"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                        <tag k='hydrologischesMerkmal' v='2000' />
+                    </way>
+                    <way id="-2" version="0">
+                        <nd ref="-4"/>
+                        <nd ref="-9"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                        <tag k='hydrologischesMerkmal' v='2000' />
+                    </way>
+                    <relation id='-3' changeset='-1'>
+                        <member type='way' ref='-1' role='' />
+                        <member type='way' ref='-2' role='' />
+                        <tag k="object_type" v="AX_Wasserlauf"/>
+                        <tag k="widmung" v="1310"/>
+                        <tag k='gewaesserkennzahl' v='1' />
+                    </relation>
+                </create>
+                <modify/>
+                <delete if-unused="true"/>
+            </osmChange>
+            """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createWasserlaufMitWidmungOhneHydrologischesMerkmal() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                <create>
+                    <node id="-1" lon="12.33103749863201" lat="49.87911888618271" version="0"/>
+                    <node id="-2" lon="12.331030993733307" lat="49.878754209010715" version="0"/>
+                    <node id="-4" lon="12.331021236757456" lat="49.87835809096834" version="0"/>
+                    <node id="-7" lon="12.33103432094183" lat="49.87894073908409" version="0"/>
+                    <node id="-9" lon="12.331024850360901" lat="49.87850479802609" version="0"/>
+                    <way id="-1" version="0">
+                        <nd ref="-1"/>
+                        <nd ref="-7"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                    </way>
+                    <way id="-2" version="0">
+                        <nd ref="-4"/>
+                        <nd ref="-9"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                    </way>
+                    <relation id='-3' changeset='-1'>
+                        <member type='way' ref='-1' role='' />
+                        <member type='way' ref='-2' role='' />
+                        <tag k="object_type" v="AX_Wasserlauf"/>
+                        <tag k="widmung" v="1310"/>
+                    </relation>
+                </create>
+                <modify/>
+                <delete if-unused="true"/>
+            </osmChange>
+            """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createWasserlaufMitWidmungMitHydrologischesMerkmal() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                <create>
+                    <node id="-1" lon="12.33103749863201" lat="49.87911888618271" version="0"/>
+                    <node id="-2" lon="12.331030993733307" lat="49.878754209010715" version="0"/>
+                    <node id="-4" lon="12.331021236757456" lat="49.87835809096834" version="0"/>
+                    <node id="-7" lon="12.33103432094183" lat="49.87894073908409" version="0"/>
+                    <node id="-9" lon="12.331024850360901" lat="49.87850479802609" version="0"/>
+                    <way id="-1" version="0">
+                        <nd ref="-1"/>
+                        <nd ref="-7"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                        <tag k='hydrologischesMerkmal' v='2000' />
+                    </way>
+                    <way id="-2" version="0">
+                        <nd ref="-4"/>
+                        <nd ref="-9"/>
+                        <nd ref="-2"/>
+                        <tag k="object_type" v="AX_Gewaesserachse"/>
+                        <tag k='breiteDesGewaessers' v='12' />
+                        <tag k='fliessrichtung' v='TRUE' />
+                    </way>
+                    <relation id='-3' changeset='-1'>
+                        <member type='way' ref='-1' role='' />
+                        <member type='way' ref='-2' role='' />
+                        <tag k="object_type" v="AX_Wasserlauf"/>
+                        <tag k="widmung" v="1310"/>
+                    </relation>
+                </create>
+                <modify/>
+                <delete if-unused="true"/>
+            </osmChange>
+            """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto attributeCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "attribute-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'attribute-check' not found"));
+
+        assertThat(attributeCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(attributeCheck.errors())
+                .as("Errors of 'attribute-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(attributeCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'attribut-check'")
+                .contains("Das Tag 'widmung' wird nur belegt, wenn 'gewaesserkennzahl' belegt ist und/oder die zur Relation gehörenden 'AX_Gewaesserachse' nicht das Tag 'hydrologischesMerkmal' führen.");
+    }
+}
