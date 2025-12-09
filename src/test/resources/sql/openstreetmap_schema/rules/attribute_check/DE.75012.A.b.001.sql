2fa7a43fb96b908e8d79d7e4f59d1321a839f961
Tobias Walter <tob-walter@gmx.de>
2025-12-09 14:53:03 +0100

    Add test case DE.75012.A.b.001

--- src/test/resources/sql/openstreetmap_schema/rules/attribute_check/DE.75012.A.b.001.sql
+++ src/test/resources/sql/openstreetmap_schema/rules/attribute_check/DE.75012.A.b.001.sql
@@ -4,16 +4,16 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
     'AX_KommunalesTeilgebiet',
     '{
         "checks": {
-            "type": "object_exists",
-            "data_set_filter": {
-                "criteria": {
+            "relations": {
+                "conditions": { "type": "tag_equals", "tag_key": "object_type", "value": "AX_Gemeindeteil" },
+                "checks": {
                     "all": [
-                        { "type": "tag_equals", "tag_key": "object_type", "value": "AX_Gemeindeteil" },
-                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:land", "value": "current:kennzeichen:land" },
-                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:regierungsbezirk", "value": "current:kennzeichen:regierungsbezirk" },
-                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:kreis", "value": "current:kennzeichen:kreis" },
-                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:gemeinde", "value": "current:kennzeichen:gemeinde" },
-                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:gemeindeteil", "value": "current:kennzeichen:gemeindeteil" }
+                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:land", "value": "base:kennzeichen:land" },
+                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:regierungsbezirk", "value": "base:kennzeichen:regierungsbezirk" },
+                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:kreis", "value": "base:kennzeichen:kreis" },
+                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:gemeinde", "value": "base:kennzeichen:gemeinde" },
+                        { "type": "tag_equals", "tag_key": "gemeindekennzeichen:gemeindeteil", "value": "base:kennzeichen:gemeindeteil" },
+                        { "type": "tag_equals", "tag_key": "schluesselGesamt", "value": "base:schluesselGesamt" }
                     ]
                 }
             }
