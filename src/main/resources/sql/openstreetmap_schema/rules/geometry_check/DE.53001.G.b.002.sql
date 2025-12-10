6dd0cb09b1be38ec77c37d6d6fba7bcd04116ec1
Tobias Walter <tob-walter@gmx.de>
2025-12-10 12:50:12 +0100

    Add test case DE.53001.G.b.002

--- src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.53001.G.b.002.sql
+++ src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.53001.G.b.002.sql
@@ -12,6 +12,7 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
         "checks": {
             "type": "spatial_compare",
             "operator": "equals_topo",
+            "aggregator": "union",
             "data_set_filter": {
                 "criteria": {
                     "type": "tag_in", "tag_key": "object_type", "values": ["AX_Strassenachse", "AX_Fahrbahnachse", "AX_Fahrwegachse",
