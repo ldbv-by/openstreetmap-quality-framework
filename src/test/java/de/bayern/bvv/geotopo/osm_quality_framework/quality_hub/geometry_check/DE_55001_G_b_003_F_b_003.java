fad51054600486828bbf6e3e86098888fbde24a4
Tobias Walter <tob-walter@gmx.de>
2025-12-10 19:27:22 +0100

    Add test case DE.55001.G.b.003 and DE.55001.F.b.003

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/geometry_check/DE_55001_G_b_003_F_b_003.java
@@ -0,0 +1,151 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.geometry_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * 55001 'Gewässermerkmal' mit ART 1620 'Wasserfall' darf nur innerhalb 44001 'Fließgewässer' (ohne FKT 8300)
+ * oder auf 44004 'Gewässerachse' (ohne FKT) geometrieidentisch liegen.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_55001_G_b_003_F_b_003 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("geometry-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.55001.G.b.003_F.b.003"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createWasserfallAufGewaesserachseOhneFunktion() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25366' changeset='-1' lat='49.88418770251' lon='12.32312094704' />
+                  <node id='-25365' changeset='-1' lat='49.88279744114' lon='12.32304388852' />
+                  <way id='-667' changeset='-1'>
+                    <nd ref='-25366' />
+                    <nd ref='-25365' />
+                    <tag k='object_type' v='AX_Gewaessermerkmal' />
+                    <tag k='art' v='1620' />
+                  </way>
+                  <way id='-663' changeset='-1'>
+                    <nd ref='-25366' />
+                    <nd ref='-25365' />
+                    <tag k='object_type' v='AX_Gewaesserachse' />
+                    <tag k='breiteDesGewaessers' v='12' />
+                    <tag k='fliessrichtung' v='TRUE' />
+                  </way>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createWasserfallAufGewaesserachseMitFunktion() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25366' changeset='-1' lat='49.88418770251' lon='12.32312094704' />
+                  <node id='-25365' changeset='-1' lat='49.88279744114' lon='12.32304388852' />
+                  <way id='-667' changeset='-1'>
+                    <nd ref='-25366' />
+                    <nd ref='-25365' />
+                    <tag k='object_type' v='AX_Gewaessermerkmal' />
+                    <tag k='art' v='1620' />
+                  </way>
+                  <way id='-663' changeset='-1'>
+                    <nd ref='-25366' />
+                    <nd ref='-25365' />
+                    <tag k='object_type' v='AX_Gewaesserachse' />
+                    <tag k='funktion' v='8300' />
+                    <tag k='breiteDesGewaessers' v='12' />
+                    <tag k='fliessrichtung' v='TRUE' />
+                  </way>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Ein linienförmiges Objekt 'AX_Gewaessermerkmal' mit 'art' 1620 darf nur innerhalb von 'AX_Fliessgewaesser' ohne 'funktion' 8300 oder geometrieidentisch auf 'AX_Gewaesserachse' ohne 'funktion' 8300 liegen.");
+    }
+}
