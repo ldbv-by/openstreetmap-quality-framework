90d59e4cf19721cd641d54006fd180136c1b7f62
Tobias Walter <tob-walter@gmx.de>
2025-12-10 13:17:56 +0100

    Add test case DE.53002.G.b.003

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/geometry_check/DE_53002_G_b_003.java
@@ -0,0 +1,159 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.geometry_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * Bei punktförmiger Modellierung der Werteart 2000 der Attributart 'Art' liegt die 'Furt' immer im Schnittpunkt eines
+ * Objekts 44004 'Gewässerachse' mit 42003 'Straßenachse', 42008 'Fahrwegachse' oder 53003 'Weg, Pfad, Steig'.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_53002_G_b_003 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("geometry-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.53002.G.b.003"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createFurtAmSchnittpunktVonGewaesserachseUndStrassenachse() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25434' changeset='-1' lat='49.88561514895' lon='12.32092479857' />
+                  <node id='-25432' changeset='-1' lat='49.8855530864' lon='12.32664639346' />
+                  <node id='-25430' changeset='-1' lat='49.88364152076' lon='12.32354478815' />
+                  <node id='-25429' changeset='-1' lat='49.88741492821' lon='12.32352552352' />
+                  <node id='-25433' changeset='-1' lat='49.88559042534' lon='12.32353483847'>
+                    <tag k='object_type' v='AX_Strassenverkehrsanlage' />
+                    <tag k='art' v='2000' />
+                  </node>
+                  <way id='-802' changeset='-1'>
+                    <nd ref='-25432' />
+                    <nd ref='-25433' />
+                    <nd ref='-25434' />
+                    <tag k='object_type' v='AX_Strassenachse' />
+                    <tag k='breiteDerFahrbahn' v='9' />
+                  </way>
+                  <way id='-784' changeset='-1'>
+                    <nd ref='-25429' />
+                    <nd ref='-25433' />
+                    <nd ref='-25430' />
+                    <tag k='object_type' v='AX_Gewaesserachse' />
+                    <tag k='breiteDesGewaessers' v='12' />
+                    <tag k='fliessrichtung' v='TRUE' />
+                  </way>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createFurtAufGewaesserachse() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-25434' changeset='-1' lat='49.88561514895' lon='12.32092479857' />
+                  <node id='-25432' changeset='-1' lat='49.8855530864' lon='12.32664639346' />
+                  <node id='-25430' changeset='-1' lat='49.88364152076' lon='12.32354478815' />
+                  <node id='-25429' changeset='-1' lat='49.88741492821' lon='12.32352552352' />
+                  <node id='-25433' changeset='-1' lat='49.88559042534' lon='12.32353483847'>
+                    <tag k='object_type' v='AX_Strassenverkehrsanlage' />
+                    <tag k='art' v='2000' />
+                  </node>
+                  <way id='-784' changeset='-1'>
+                    <nd ref='-25429' />
+                    <nd ref='-25433' />
+                    <nd ref='-25430' />
+                    <tag k='object_type' v='AX_Gewaesserachse' />
+                    <tag k='breiteDesGewaessers' v='12' />
+                    <tag k='fliessrichtung' v='TRUE' />
+                  </way>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Das Objekt mit 'art' 2000 liegt am Schnittpunkt einer 'AX_Gewaesserachse' mit 'AX_Strassenachse', 'AX_Fahrwegachse' oder 'AX_WegPfadSteig'.");
+    }
+}
