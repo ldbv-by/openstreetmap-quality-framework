2151bf488874835caf70c7961930b1c9e57ab17f
Tobias Walter <tob-walter@gmx.de>
2025-12-11 11:01:59 +0100

    Add test case DE.75003.G.a.003

--- src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.75003.G.a.003.sql
+++ src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.75003.G.a.003.sql
@@ -1,5 +1,5 @@
 INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error_text) VALUES (
-    'DE.DE.75003.G.a.003',
+    'DE.75003.G.a.003',
     'geometry-check',
     'AX_KommunalesGebiet',
     '{
@@ -16,7 +16,22 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
                 {
                     "type": "spatial_compare",
                     "operator": "surrounded_by",
-                    "data_set_filter": { "featureFilter": { "tags": { "object_type": "AX_KommunalesGebiet|AX_Gebietsgrenze", "artDerGebietsgrenze": "not_exists|7101|7102" } } }
+                    "aggregator": "union",
+                    "data_set_filter": {
+                        "criteria": {
+                            "any": [
+                                {
+                                    "type": "tag_equals", "tag_key": "object_type", "value": "AX_KommunalesGebiet"
+                                },
+                                {
+                                    "all": [
+                                         { "type": "tag_equals", "tag_key": "object_type", "value": "AX_Gebietsgrenze" },
+                                         { "type": "tag_in", "tag_key": "artDerGebietsgrenze", "values": ["7101", "7102"] }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
                 }
             ]
         }
