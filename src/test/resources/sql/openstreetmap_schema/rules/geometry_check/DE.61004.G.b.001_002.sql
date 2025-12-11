db5d237a827010854032904ce6a74a71a8da8b5a
Tobias Walter <tob-walter@gmx.de>
2025-12-11 16:18:54 +0100

    Add test case DE.61004.G.b.001 and DE.61004.G.b.002

--- src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.61004.G.b.001_002.sql
+++ src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.61004.G.b.001_002.sql
@@ -7,7 +7,10 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
         "checks": {
             "type": "spatial_compare",
             "operator": "equals_topo",
-            "data_set_filter": { "featureFilter": { "tags": { "object_type": "AX_Strassenachse|AX_Fahrbahnachse|AX_Fahrwegachse|AX_Bahnstrecke|AX_WegPfadSteig" } } }
+            "data_set_filter": {
+                "aggregator": "union",
+                "criteria": { "type": "tag_in", "tag_key": "object_type", "values": ["AX_Strassenachse", "AX_Fahrbahnachse", "AX_Fahrwegachse", "AX_Bahnstrecke", "AX_WegPfadSteig"] }
+            }
         }
     }',
     'Ein Objekt ''AX_Einschnitt'' mit ''funktion'' 3002 oder 3004 überlagert immer ein Objekt ''AX_Strassenachse'', ''AX_Fahrbahnachse'', ''AX_Fahrwegachse'', ''AX_Bahnstrecke'' oder ''AX_WegPfadSteig'' mit identischer Geometrie.')
