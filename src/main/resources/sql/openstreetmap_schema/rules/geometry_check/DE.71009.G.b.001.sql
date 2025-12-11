86a642b1b4a72e829c0d05a29d8fa89aa7e82962
Tobias Walter <tob-walter@gmx.de>
2025-12-11 14:29:13 +0100

    Add test case DE.71009.G.b.001

--- src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.71009.G.b.001.sql
+++ src/main/resources/sql/openstreetmap_schema/rules/geometry_check/DE.71009.G.b.001.sql
@@ -5,18 +5,20 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
     '{
         "conditions": { "type": "tag_equals", "tag_key": "artDerFestlegung", "value": "2711" },
         "checks": {
-            "any": [
-                {
-                    "type": "spatial_compare",
-                    "operator": "equals_topo",
-                    "data_set_filter": { "featureFilter": { "tags": { "object_type": "AX_Gebaeude|AX_Turm|AX_HistorischesBauwerkOderHistorischeEinrichtung" } } }
-                },
-                {
-                    "type": "spatial_compare",
-                    "operator": "equals_topo",
-                    "data_set_filter": { "featureFilter": { "tags": { "object_type": "AX_SonstigesBauwerkOderSonstigeEinrichtung", "bauwerksfunktion": "1750|1760|1770" } } }
+            "type": "spatial_compare",
+            "operator": "equals_topo",
+            "data_set_filter": {
+                "criteria": {
+                    "any": [
+                        { "type": "tag_in", "tag_key": "object_type", "values": ["AX_Gebaeude", "AX_Turm", "AX_HistorischesBauwerkOderHistorischeEinrichtung"] },
+                        { "all": [
+                             { "type": "tag_equals", "tag_key": "object_type", "value": "AX_SonstigesBauwerkOderSonstigeEinrichtung" },
+                             { "type": "tag_in", "tag_key": "bauwerksfunktion", "values": ["1750", "1760", "1770"] }
+                          ]
+                        }
+                    ]
                 }
-            ]
+            }
         }
     }',
     'Ein Objekt ''AX_Denkmalschutzrecht'' mit ''artDerFestlegung'' 2711 muss geometrieidentisch mit einem Objekt ''AX_Gebaeude'', ''AX_Turm'', ''AX_HistorischesBauwerkOderHistorischeEinrichtung'' oder ''AX_SonstigesBauwerkOderSonstigeEinrichtung'' mit ''bauwerksfunktion'' 1750, 1760 oder 1770 sein.')
---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/geometry_check/DE_71009_G_b_001.java
@@ -0,0 +1,133 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.geometry_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * Objekte mit artDerFestlegung 2711 Baudenkmal müssen geometrieidentisch mit einem Objekt 31001 'Gebäude',
+ * 51001 'Turm', 51007 'Historisches Bauwerk oder historische Einrichtung' oder
+ * 51009 'Sonstiges Bauwerk oder sonstige Einrichtung' mit BWF 1750, 1760 oder 1770 sein.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_71009_G_b_001 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("geometry-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.71009.G.b.001"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createBaudenkmalAufTurm() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-1' changeset='-1' lat='49.88567721142' lon='12.33907207933'>
+                    <tag k='object_type' v='AX_Denkmalschutzrecht' />
+                    <tag k='artDerFestlegung' v='2711' />
+                  </node>
+                  <node id='-25402' changeset='-1' lat='49.88567721142' lon='12.33907207933'>
+                    <tag k='object_type' v='AX_Turm' />
+                    <tag k='bauwerksfunktion' v='1003' />
+                  </node>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createBaudenkmalOhneTurm() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="JOSM">
+                <create>
+                  <node id='-1' changeset='-1' lat='49.88567721142' lon='12.33907207933'>
+                    <tag k='object_type' v='AX_Denkmalschutzrecht' />
+                    <tag k='artDerFestlegung' v='2711' />
+                  </node>
+                </create>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Ein Objekt 'AX_Denkmalschutzrecht' mit 'artDerFestlegung' 2711 muss geometrieidentisch mit einem Objekt 'AX_Gebaeude', 'AX_Turm', 'AX_HistorischesBauwerkOderHistorischeEinrichtung' oder 'AX_SonstigesBauwerkOderSonstigeEinrichtung' mit 'bauwerksfunktion' 1750, 1760 oder 1770 sein.");
+    }
+}
