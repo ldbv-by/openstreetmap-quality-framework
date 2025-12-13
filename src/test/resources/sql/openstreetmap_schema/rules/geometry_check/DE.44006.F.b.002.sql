ea92b69c6fbbc4deeee5b50a7a173d46cb9b3460
Tobias Walter <tob-walter@gmx.de>
2025-12-13 17:05:43 +0100

    Add test case DE.44006.F.b.002

---
+++ src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.44006.F.b.002.sql
@@ -0,0 +1,40 @@
+INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error_text) VALUES (
+    'DE.44006.F.b.002',
+    'geometry-check',
+    'AX_StehendesGewaesser',
+    '{
+        "checks": {
+            "not": {
+                "type": "spatial_compare",
+                "operator": "intersects",
+                "data_set_filter": {
+                    "criteria": {
+                        "all": [
+                            { "type": "tag_in", "tag_key": "object_type", "values": ["AX_Strassenachse", "AX_Fahrbahnachse", "AX_Fahrwegachse", "AX_Bahnstrecke", "AX_WegPfadSteig", "AX_Gleis"] },
+                            {
+                                "not": {
+                                    "type": "relation_exists",
+                                    "criteria": { "type": "tag_equals", "tag_key": "object_type", "value": "AA_hatDirektUnten" },
+                                    "relation_members": {
+                                        "criteria": {
+                                            "any": [
+                                                { "type": "tag_in", "tag_key": "object_type", "values": ["AX_BauwerkImVerkehrsbereich", "AX_BauwerkImGewaesserbereich"] },
+                                                {
+                                                    "all": [
+                                                        { "type": "tag_equals", "tag_key": "object_type", "value": "AX_EinrichtungenFuerDenSchiffsverkehr" },
+                                                        { "type": "tag_equals", "tag_key": "art", "value": "1460" }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }',
+    'Ein Stehendes Gewässer darf keine gemeinsamen Kanten mit Verkehrsachsen führen, wenn nicht eine Relation zu einem Bauwerk besteht.')
+ON CONFLICT (id) DO NOTHING;
