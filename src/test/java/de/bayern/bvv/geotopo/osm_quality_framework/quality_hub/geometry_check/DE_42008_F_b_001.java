38a11763ccb726df473ea46964465e1e92d3c3af
Tobias Walter <tob-walter@gmx.de>
2025-12-13 15:05:24 +0100

    Add test case DE.42008.F.b.001

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/geometry_check/DE_42008_F_b_001.java
@@ -0,0 +1,145 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.geometry_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * 42008 'Fahrwegachse' muss geometrisch an mindestens eine weitere 42008 'Fahrwegachse', 42005 'Fahrbahnachse', 42003 'Straßenachse' oder
+ * 57002 'Schifffahrtslinie, Fährverkehr' anschließen oder auf 75009 'Gebietsgrenze' mit
+ * 'Art der Gebietsgrenze' 7102 'Grenze des Bundeslandes' enden.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_42008_F_b_001 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("geometry-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.42008.F.b.001"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createAngeschlosseneFahrwegachse() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                    <create>
+                        <node id="-10" lon="12.325914631370837" lat="49.87780143780461" version="0"/>
+                        <node id="-11" lon="12.325803713603845" lat="49.876800740743164" version="0"/>
+                        <node id="-13" lon="12.326752676721458" lat="49.87574442690155" version="0"/>
+                        <way id="-3" version="0">
+                            <nd ref="-10"/>
+                            <nd ref="-11"/>
+                            <tag k="object_type" v="AX_Fahrwegachse"/>
+                        </way>
+                        <way id="-4" version="0">
+                            <nd ref="-13"/>
+                            <nd ref="-11"/>
+                            <tag k="object_type" v="AX_Fahrwegachse"/>
+                        </way>
+                    </create>
+                    <modify/>
+                    <delete if-unused="true"/>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createNichtAngeschlosseneFahrwegachse() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                    <create>
+                        <node id="-10" lon="12.325914631370837" lat="49.87780143780461" version="0"/>
+                        <node id="-11" lon="12.325803713603845" lat="49.876800740743164" version="0"/>
+                        <way id="-3" version="0">
+                            <nd ref="-10"/>
+                            <nd ref="-11"/>
+                            <tag k="object_type" v="AX_Fahrwegachse"/>
+                        </way>
+                    </create>
+                    <modify/>
+                    <delete if-unused="true"/>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Fahrwegachse ist nicht an das Verkehrsnetz angeschlossen.");
+    }
+}
