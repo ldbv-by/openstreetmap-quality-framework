a635269ea1a3ca085d7e85407e94fbad775961ab
Tobias Walter <tob-walter@gmx.de>
2025-12-15 14:57:00 +0100

    Add test case DE.42014.A.b.001

--- src/main/resources/sql/openstreetmap_schema/rules/attribute_check/DE.42014.A.b.001.sql
+++ src/main/resources/sql/openstreetmap_schema/rules/attribute_check/DE.42014.A.b.001.sql
@@ -4,7 +4,7 @@ INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error
     'AX_Bahnstrecke',
     '{
         "conditions": { "type": "tag_equals", "tag_key": "spurweite", "value": "9997" },
-        "checks": { "type": "tag_in", "tag_key": "bahnkategorie", "values": ["1600"] }
+        "checks": { "type": "tag_equals", "tag_key": "bahnkategorie", "value": "1600" }
     }',
     'Das Tag ''spurweite'' mit dem Wert 9997 darf nur bei der ''bahnkategorie'' 1600 vorkommen.')
 ON CONFLICT (id) DO NOTHING;
