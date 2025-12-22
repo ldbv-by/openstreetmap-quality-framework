a5d6d7c2e0e6556047b7b6865f09f6e57b376737
Tobias Walter <tob-walter@gmx.de>
2025-12-22 09:47:12 +0100

    Add test case DE.53001.R.b.001

---
+++ src/test/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_hub/geometry_check/DE_53001_R_b_001.java
@@ -0,0 +1,222 @@
+package de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.geometry_check;
+
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.config.JtsJackson3Module;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_hub.dto.QualityHubResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceErrorDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_services.dto.QualityServiceResultDto;
+import de.bayern.bvv.geotopo.osm_quality_framework.test_core.DatabaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.MapperFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * AdV-Beschreibung:
+ * Die Regel eine Relation "hatDirektUnten" wird nur bei den Objekten geführt, die über oder unter der Erdoberfläche liegen.
+ * Diese Voraussetzung ist gegeben, wenn linienförmige Objekte(Straßenachse, Fahrwegachse, WegPfadSteig, Bahnstrecke, Gleis, Gewässerachse)  geometrieidentisch (auch verkettet)
+ * z.B. auf Bauwerken (z.B. Brücke) bzw. in Bauwerken (z.B. Tunnel) oder bei flächenförmigen Bauwerken (Straßenverkehr, Straßenachse, Fahrbahnachse, Fahrwegachse WegPfadSteig, Bahnverkehr, Bahnstrecke, Gleis, Fließgewässer, Gewässerachse) innerhalb der Umrissgeometrie des Objekts 53001 'Bauwerk im Verkehrsbereich' liegen.
+ * <p>
+ * Es ist zu prüfen, ob die OID eine Relation hatDirektUnten auf das geometrieidentische Bauwerk besitzt. Bauwerke im Verkehrsbereich mit BWF 1880 Schutzgalerie, Einhausung, 1890 Schleusenkammer und 1900 Durchfahrt dürfen nicht per hDU referenziert werden und sind deshalb bei der Prüfung zu ignorieren.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class DE_53001_R_b_001 extends DatabaseIntegrationTest {
+
+    final Long CHANGESET_ID = 1L;
+
+    Set<String> stepsToValidate = new HashSet<>(Set.of("geometry-check", "object-number-assignment"));
+    Set<String> rulesToValidate = new HashSet<>(Set.of("DE.53001.R.b.001"));
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .addModule(new JtsJackson3Module())
+            .build();
+
+    @Test
+    void createBrueckeMitStrassenachseMitHDU() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                    <create>
+                        <node id="-1" lon="12.317446189523055" lat="49.87283851843256" version="0"/>
+                        <node id="-2" lon="12.318295913469939" lat="49.872866176770636" version="0"/>
+                        <way id="-1" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_Strassenachse"/>
+                        </way>
+                        <way id="-2" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_BauwerkImVerkehrsbereich"/>
+                            <tag k="bauwerksfunktion" v="1800"/>
+                        </way>
+                        <relation id="-1" version="0">
+                            <member type="way" role="under" ref="-2"/>
+                            <member type="way" role="over" ref="-1"/>
+                            <tag k="object_type" v="AA_hatDirektUnten"/>
+                        </relation>
+                    </create>
+                    <modify/>
+                    <delete if-unused="true"/>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result to be valid, but it was invalid.").isTrue();
+    }
+
+    @Test
+    void createBrueckeMitStrassenachseOhneHDU() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                    <create>
+                        <node id="-1" lon="12.317446189523055" lat="49.87283851843256" version="0"/>
+                        <node id="-2" lon="12.318295913469939" lat="49.872866176770636" version="0"/>
+                        <way id="-1" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_Strassenachse"/>
+                        </way>
+                        <way id="-2" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_BauwerkImVerkehrsbereich"/>
+                            <tag k="bauwerksfunktion" v="1800"/>
+                        </way>
+                    </create>
+                    <modify/>
+                    <delete if-unused="true"/>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Das Bauwerk wird nicht korrekt referenziert.");
+    }
+
+    @Test
+    void createBrueckeMitGebietsgrenzeMitHDU() throws Exception {
+        // Arrange
+        final String CHANGESET_XML = """
+                <osmChange version="0.6" generator="iD">
+                    <create>
+                        <node id="-1" lon="12.317446189523055" lat="49.87283851843256" version="0"/>
+                        <node id="-2" lon="12.318295913469939" lat="49.872866176770636" version="0"/>
+                        <way id="-1" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_Gebietsgrenze"/>
+                        </way>
+                        <way id="-2" version="0">
+                            <nd ref="-1"/>
+                            <nd ref="-2"/>
+                            <tag k="object_type" v="AX_BauwerkImVerkehrsbereich"/>
+                            <tag k="bauwerksfunktion" v="1800"/>
+                        </way>
+                        <relation id="-1" version="0">
+                            <member type="way" role="under" ref="-2"/>
+                            <member type="way" role="over" ref="-1"/>
+                            <tag k="object_type" v="AA_hatDirektUnten"/>
+                        </relation>
+                    </create>
+                    <modify/>
+                    <delete if-unused="true"/>
+                </osmChange>
+                """;
+
+        // Act
+        MvcResult mvcResult = this.mockMvc.perform(
+                        post("/osm-quality-framework/v1/quality-hub/check/changeset/{id}", CHANGESET_ID)
+                                .contentType(MediaType.APPLICATION_XML)
+                                .content(CHANGESET_XML)
+                                .param("steps", String.join(",", stepsToValidate))
+                                .param("rules", String.join(",", rulesToValidate)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        QualityHubResultDto qualityHubResultDto = this.objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), QualityHubResultDto.class);
+
+        // Assert
+        assertThat(qualityHubResultDto).as("Quality-Hub result must not be null").isNotNull();
+        assertThat(qualityHubResultDto.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        QualityServiceResultDto geometryCheck = qualityHubResultDto.qualityServiceResults().stream()
+                .filter(s -> "geometry-check".equals(s.qualityServiceId()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("QualityService 'geometry-check' not found"));
+
+        assertThat(geometryCheck.isValid()).withFailMessage("Expected the result is not valid, but it was valid.").isFalse();
+
+        assertThat(geometryCheck.errors())
+                .as("Errors of 'geometry-check' must not be empty")
+                .isNotEmpty();
+
+        assertThat(geometryCheck.errors())
+                .extracting(QualityServiceErrorDto::errorText)
+                .as("Error text of 'geometry-check'")
+                .contains("Das Bauwerk wird nicht korrekt referenziert.");
+    }
+}
---
+++ src/test/resources/sql/openstreetmap_schema/rules/geometry_check/DE.53001.R.b.001.sql
@@ -0,0 +1,29 @@
+INSERT INTO openstreetmap_schema.rules (id, type, object_type, expression, error_text) VALUES (
+    'DE.53001.R.b.001',
+    'geometry-check',
+    'AX_BauwerkImVerkehrsbereich',
+    '{
+        "checks": {
+            "any": [
+                { "type": "tag_in", "tag_key": "bauwerksfunktion", "values": ["1880", "1890", "1900"] },
+                {
+                    "relations": {
+                        "loop_info": { "type": "all" },
+                        "conditions": {
+                            "type": "tag_equals", "tag_key": "object_type", "value": "AA_hatDirektUnten"
+                        },
+                        "checks": {
+                            "relation_members": {
+                                "loop_info": { "type": "all" },
+                                "checks": { "type": "tag_in", "tag_key": "object_type", "values": ["AX_Strassenachse", "AX_Fahrwegachse", "AX_WegPfadSteig", "AX_Bahnstrecke", "AX_Gleis",
+                                                                                                  "AX_Gewaesserachse", "AX_Strassenverkehr", "AX_Bahnverkehr", "AX_Fliessgewaesser",
+                                                                                                  "AX_BauwerkImVerkehrsbereich"] }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }',
+    'Das Bauwerk wird nicht korrekt referenziert.')
+ON CONFLICT (id) DO NOTHING;
