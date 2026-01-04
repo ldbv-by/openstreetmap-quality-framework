91441fbd6e6d887eff480899d36f0de17c46712f
Tobias Walter <tob-walter@gmx.de>
2026-01-04 20:15:26 +0100

    Check for deleted objects all neighbour objects

--- src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_services/geometry_check/service/GeometryCheckService.java
+++ src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_services/geometry_check/service/GeometryCheckService.java
@@ -1,10 +1,12 @@
 package de.bayern.bvv.geotopo.osm_quality_framework.quality_services.geometry_check.service;
 
+import de.bayern.bvv.geotopo.osm_quality_framework.geodata_view.api.GeodataViewService;
 import de.bayern.bvv.geotopo.osm_quality_framework.openstreetmap_schema.api.OsmSchemaService;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.dto.DataSetDto;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.mapper.ChangesetDataSetMapper;
-import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.model.ChangesetDataSet;
-import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.model.Feature;
-import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.model.TaggedObject;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.mapper.DataSetMapper;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.mapper.FeatureMapper;
+import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.dataset.model.*;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.object_type.mapper.ObjectTypeMapper;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.object_type.model.ObjectType;
 import de.bayern.bvv.geotopo.osm_quality_framework.quality_core.object_type.model.Rule;
@@ -32,6 +34,7 @@ public class GeometryCheckService implements QualityService {
 
     private final OsmSchemaService osmSchemaService;
     private final RuleEngine ruleEngine;
+    private final GeodataViewService geodataViewService;
 
     /**
      * Executes the quality check for the given request.
@@ -67,6 +70,39 @@ public class GeometryCheckService implements QualityService {
 
                 if (!this.ruleEngine.evaluate(new RuleEvaluationDto(changedObject, rule))) {
                     this.setError(changedObject, rule.getErrorText());
+                }
+            }
+        }
+
+        // ----- Check the geometric consistency for neighbour objects of deleted changeset object.
+        for (TaggedObject deletedObject : changesetDataSet.getDelete().getAll()) {
+
+            if (deletedObject instanceof Feature deletedFeature) {
+
+                DataSet neighbourObjects = DataSetMapper.toDomain(this.geodataViewService.getDataSetBySpatialRelation(
+                        FeatureMapper.toDto(deletedFeature), Set.of(SpatialOperator.INTERSECTS),
+                        new DataSetFilter(false, null, null, null, null, null),
+                        false
+                ));
+
+                for (TaggedObject neighbourObject : neighbourObjects.getAll()) {
+                    // ----- Get rules from openstreetmap-schema for the object_type.
+                    List<Rule> rules = Optional.ofNullable(
+                                    this.osmSchemaService.getObjectTypeInfo(neighbourObject.getObjectType()))
+                            .map(ObjectTypeMapper::toDomain)
+                            .map(ObjectType::getRules)
+                            .orElse(List.of());
+
+                    // ----- Check geometry-check rules for changed object.
+                    for (Rule rule : rules.stream()
+                            .filter(r -> QUALITY_SERVICE_NAME.equals(r.getType()))
+                            .filter(r -> !hasRuleFilter || rulesToValidate.contains(r.getId()))
+                            .toList()) {
+
+                        if (!this.ruleEngine.evaluate(new RuleEvaluationDto(neighbourObject, rule))) {
+                            this.setError(deletedObject, rule.getErrorText());
+                        }
+                    }
                 }
             }
         }
