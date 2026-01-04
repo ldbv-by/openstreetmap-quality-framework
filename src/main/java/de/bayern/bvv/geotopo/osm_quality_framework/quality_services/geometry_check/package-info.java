6ccb9d7552cd80f6cdd8aee4a5eed59dd86094f5
Tobias Walter <tob-walter@gmx.de>
2026-01-04 22:16:05 +0100

    Add missing bounded context dependency

--- src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_services/geometry_check/package-info.java
+++ src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/quality_services/geometry_check/package-info.java
@@ -3,7 +3,8 @@
                 "quality_services :: *",
                 "openstreetmap_schema :: *",
                 "rule_engine :: *",
-                "quality_core :: *"
+                "quality_core :: *",
+                "geodata_view :: *"
         }
 )
 package de.bayern.bvv.geotopo.osm_quality_framework.quality_services.geometry_check;
