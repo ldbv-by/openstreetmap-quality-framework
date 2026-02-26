fd18ead6bc801ffdb27907211845252c250c7a85
Tobias Walter <tob-walter@gmx.de>
2026-02-26 12:30:17 +0100

    presets not exists

--- src/main/java/de/bayern/bvv/geotopo/osm_quality_framework/presets/package-info.java
+++
@@ -1,7 +0,0 @@
-@org.springframework.modulith.ApplicationModule(
-        allowedDependencies = {
-                "quality_core :: *",
-                "openstreetmap_schema :: *"
-        }
-)
-package de.bayern.bvv.geotopo.osm_quality_framework.presets;
